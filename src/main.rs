57948e35bde7dcc8245ca1d09b547afd57e302c5
Jacob <68519879+aufdj@users.noreply.github.com>
2023-04-05 00:04:40 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,81 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Write, BufReader, BufWriter, BufRead, Read, Seek, SeekFrom};
+use std::fs::{File, metadata};
+use std::io::{Write, BufReader, BufWriter, BufRead, Read};
 use std::path::Path;
 use std::time::Instant;
 
-// Indicates an empty or non-empty buffer. 
-#[derive(PartialEq, Eq)]
-pub enum BufferState {
-    NotEmpty,
-    Empty,
-}
-
-/// A trait for handling buffered reading.
 pub trait BufferedRead {
     fn read_byte(&mut self) -> u8;
     fn read_u16(&mut self) -> u16;
+    fn read_byte_checked(&mut self) -> Option<u8>;
+    fn read_u16_checked(&mut self) -> Option<u16>;
 }
 impl BufferedRead for BufReader<File> {
-    /// Read one byte from an input file.
     fn read_byte(&mut self) -> u8 {
         let mut byte = [0u8; 1];
-        match self.read(&mut byte) {
-            Ok(_)  => {},
-            Err(e) => {
-                println!("Function read_byte failed.");
-                println!("Error: {}", e);
-            },
-        };
+        self.read(&mut byte).unwrap();
         if self.buffer().is_empty() {
             self.consume(self.capacity());
-            match self.fill_buf() {
-                Ok(_)  => {},
-                Err(e) => {
-                    println!("Function read_byte failed.");
-                    println!("Error: {}", e);
-                },
-            }
+            self.fill_buf().unwrap();
         }
         u8::from_le_bytes(byte)
     }
+
+    fn read_byte_checked(&mut self) -> Option<u8> {
+        let mut byte = [0u8; 1];
+
+        let bytes_read = self.read(&mut byte).unwrap();
+        if self.buffer().len() <= 0 { 
+            self.consume(self.capacity()); 
+            self.fill_buf().unwrap();
+        }
+        if bytes_read == 0 {
+            return None;
+        }
+        Some(u8::from_le_bytes(byte))
+    }
+
     fn read_u16(&mut self) -> u16 {
         let mut bytes = [0u8; 2];
-        let len = match self.read(&mut bytes) {
-            Ok(len)  => { len },
-            Err(e) => {
-                println!("Function read_u16 failed.");
-                println!("Error: {}", e);
-                0
-            },
-        };
+        let len = self.read(&mut bytes).unwrap();
         if self.buffer().is_empty() {
             self.consume(self.capacity());
-            match self.fill_buf() {
-                Ok(_)  => {},
-                Err(e) => {
-                    println!("Function read_u16 failed.");
-                    println!("Error: {}", e);
-                },
-            }
-            if len < 4 {
+            self.fill_buf().unwrap();
+            if len < 2 {
                 self.read_exact(&mut bytes[len..]).unwrap();
             }
         }
         u16::from_le_bytes(bytes)
     }
+
+    fn read_u16_checked(&mut self) -> Option<u16> {
+        let mut bytes = [0u8; 2];
+        let len = self.read(&mut bytes).unwrap();
+        if self.buffer().is_empty() {
+            self.consume(self.capacity());
+            self.fill_buf().unwrap();
+            if len < 2 {
+                if self.read_exact(&mut bytes[len..]).is_err() {
+                    return None;
+                }
+            }
+        }
+        Some(u16::from_le_bytes(bytes))
+    }
 }
 
-/// A trait for handling buffered writing.
 pub trait BufferedWrite {
     fn write_u16(&mut self, output: u16);
 }
 impl BufferedWrite for BufWriter<File> {
     fn write_u16(&mut self, output: u16) {
-        match self.write(&output.to_le_bytes()[..]) {
-            Ok(_)  => {},
-            Err(e) => {
-                println!("Function write_u16 failed.");
-                println!("Error: {}", e);
-            },
-        }
+        self.write(&output.to_le_bytes()[..]).unwrap();
         if self.buffer().len() >= self.capacity() {
-            match self.flush() {
-                Ok(_)  => {},
-                Err(e) => {
-                    println!("Function write_u16 failed.");
-                    println!("Error: {}", e);
-                },
-            }
+            self.flush().unwrap();
         }
     }
 }
 
-
-/// Takes a file path and returns an input file wrapped in a BufReader.
 pub fn new_input_file(capacity: usize, path: &Path) -> BufReader<File> {
     BufReader::with_capacity(
         capacity, 
@@ -100,7 +83,6 @@ pub fn new_input_file(capacity: usize, path: &Path) -> BufReader<File> {
     )
 }
 
-/// Takes a file path and returns an output file wrapped in a BufWriter.
 pub fn new_output_file(capacity: usize, path: &Path) -> BufWriter<File> {
     BufWriter::with_capacity(
         capacity, 
@@ -108,91 +90,65 @@ pub fn new_output_file(capacity: usize, path: &Path) -> BufWriter<File> {
     )
 }
 
-fn file_len(path: &Path) -> u64 {
-    path.metadata().unwrap().len()
-}
+const MAX_CODE: u16 = 65535;
 
 fn compress(mut file_in: BufReader<File>, mut file_out: BufWriter<File>) {
-    let size_in = file_in.seek(SeekFrom::End(0)).unwrap();
-    file_in.seek(SeekFrom::Start(0)).unwrap();
+    let mut eof = false;
+    let mut dict_code = 256;
 
-    let max_code: u16 = 65535;
-    let mut dict: HashMap<Vec<u8>, u16> = HashMap::new();
-
-    let mut dict_code = 257;
-    for i in 0..256 { 
-        dict.insert(vec![i as u8], i); 
-    }
+    let mut dict = (0..256)
+    .map(|i| (vec![i as u8], i))
+    .collect::<HashMap<Vec<u8>, u16>>();
     
-    
-    let mut curr_string: Vec<u8> = vec![file_in.read_byte()]; 
+    let mut string = vec![file_in.read_byte()]; 
 
-    let mut file_pos = 1;
-
-    loop {
-        while dict.contains_key(&curr_string) {
-            curr_string.push(file_in.read_byte()); 
-            file_pos += 1; 
-            if file_pos >= size_in { break; }   
-        }
-        if dict_code <= max_code {
-            dict.insert(curr_string.clone(), dict_code); 
-            dict_code+=1;
-        }
-
-        let last_char = curr_string.pop().unwrap();
-        let code = *dict.get(&curr_string).unwrap();
-        file_out.write_u16(code);
-
-        curr_string.clear();
-        curr_string.push(last_char); 
-
-        if dict_code >= max_code {
-            dict_code = 257;
-            dict.clear(); 
-            for i in 0..256 {
-                dict.insert(vec![i as u8], i);
+    while !eof {
+        while dict.contains_key(&string) {
+            if let Some(byte) = file_in.read_byte_checked() {
+                string.push(byte); 
             }
+            else {
+                eof = true;
+                break;
+            }  
         }
-        if file_pos >= size_in { break; } 
+        dict.insert(string.clone(), dict_code); 
+        dict_code += 1;
+
+        let last_char = string.pop().unwrap();
+        file_out.write_u16(*dict.get(&string).unwrap());
+
+        string.clear();
+        string.push(last_char); 
+
+        if dict_code >= MAX_CODE {
+            dict_code = 256;
+            dict.retain(|_, i| *i < 256);
+        }
     }
-    if !curr_string.is_empty() {
-        let code = *dict.get(&curr_string).unwrap();
-        file_out.write_u16(code);
-        file_out.flush().unwrap();
-    } 
-    println!("Finished compressing.");
+    if !string.is_empty() {
+        file_out.write_u16(*dict.get(&string).unwrap());
+    }
+    file_out.flush().unwrap();
 }
 
 fn decompress(mut file_in: BufReader<File>, mut file_out: BufWriter<File>) {
-    let size_in = file_in.seek(SeekFrom::End(0)).unwrap();
-    file_in.seek(SeekFrom::Start(0)).unwrap();
+    let mut dict_code = 256;
+    
+    let mut dict = (0..256)
+    .map(|i| (i, vec![i as u8]))
+    .collect::<HashMap<u16, Vec<u8>>>();
 
-    let max_code: u16 = 65535;
-    let mut dict: HashMap<u16, Vec<u8>> = HashMap::new();
+    let mut prev_string = Vec::<u8>::with_capacity(64);
 
-    let mut dict_code = 257;
-    for i in 0..256 {
-        dict.insert(i, vec![i as u8]);
-    }
-
-    let mut prev_string: Vec<u8> = Vec::with_capacity(64);
-
-    let mut file_pos = 0;
-
-    loop { 
-        let code = file_in.read_u16();
-        file_pos += 2;
-
-        if !dict.contains_key(&code) && dict_code < max_code { // Didn't recognize code
+    while let Some(code) = file_in.read_u16_checked() {
+        if !dict.contains_key(&code) {
             prev_string.push(prev_string[0]);
-            
             dict.insert(code, prev_string);
             dict_code += 1;      
         }
-        else if !prev_string.is_empty() && dict_code < max_code {
+        else if !prev_string.is_empty() {
             prev_string.push((&dict.get(&code).unwrap())[0]);
-            
             dict.insert(dict_code, prev_string);
             dict_code += 1;
         }
@@ -202,16 +158,11 @@ fn decompress(mut file_in: BufReader<File>, mut file_out: BufWriter<File>) {
 
         prev_string = string.to_vec();
         
-        if dict_code >= max_code {
-            dict_code = 257;
-            dict.clear(); 
-            for i in 0..256 {
-                dict.insert(i, vec![i as u8]);
-            }
+        if dict_code >= MAX_CODE {
+            dict_code = 256;
+            dict.retain(|i, _| *i < 256);
         }
-        if file_pos >= size_in { break; }
-    } 
-    println!("Finished Decompressing.");    
+    }  
 }
 
 fn main() {
@@ -220,13 +171,21 @@ fn main() {
     let file_in  = new_input_file(4096, Path::new(&args[1]));
     let file_out = new_output_file(4096, Path::new(&args[2]));
     match (&args[0]).as_str() {
-        "c" => compress(file_in, file_out),
-        "d" => decompress(file_in, file_out),
-        _ => println!("Enter 'c' to compress and 'd' to decompress."),
+        "c" => {
+            compress(file_in, file_out);
+            println!("Finished compressing.");
+        }
+        "d" => {
+            decompress(file_in, file_out);
+            println!("Finished Decompressing.");   
+        }
+        _ => {
+            println!("Enter 'c' to compress and 'd' to decompress.");
+        }
     } 
-
-    let size_in  = file_len(Path::new(&args[1]));
-    let size_out = file_len(Path::new(&args[2]));
     println!("{} bytes -> {} bytes in {:.2?}", 
-        size_in, size_out, time.elapsed()); 
+        metadata(Path::new(&args[1])).unwrap().len(), 
+        metadata(Path::new(&args[2])).unwrap().len(), 
+        time.elapsed()
+    ); 
 }
